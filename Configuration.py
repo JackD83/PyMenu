d409577dd9fd7cc24dfff2f62d7118b3e1fd15a3
mai <sebastian.maier@iosb.fraunhofer.de>
2018-11-22 14:00:03 +0100

    reworked configuration:
    splitted all configurations in single files
    Filenames are only used for sorting

--- Configuration.py
+++ Configuration.py
@@ -1,33 +1,88 @@
-import json, subprocess
+import json, subprocess, os, copy
 from ast import literal_eval as make_tuple
 from pprint import pprint
 
-configuration = json.load(open('config/config.json'))
+configuration = None
 theme = json.load(open('theme/theme.json'))
 listeners = []
 
-def getConfiguration():
+def getConfiguration():   
+    if(configuration == None):      
+        reloadConfiguration()
+
     return configuration
 
-def reloadConfiguration():
+def reloadConfiguration(): 
+    global configuration
     configuration = json.load(open('config/config.json'))
+    configuration["mainMenu"] = []
+    if os.path.exists(os.path.dirname("config/main")):       
+        for name in os.listdir("config/main"):
+            try:                   
+                entry = json.load(open("config/main/" + name + "/config.json"))
+                configuration["mainMenu"].append(entry)
+                if(entry["type"] == "native"):
+                    entry["data"] = []
+                    try:
+                        for itemName in os.listdir("config/main/" + name + "/items"):
+                            item = json.load(open("config/main/" + name + "/items/" + itemName))
+                            entry["data"].append(item)
+                    except Exception as ex:
+                        print(str(ex))
+
+            except Exception as ex:
+                print(str(ex))                  
+   
+  
 
 def saveConfiguration():
     try:
         subprocess.Popen(["sync"])
     except Exception:
         pass
-
-    with open('config/config.json', 'w') as fp:
-        json.dump(configuration, fp,sort_keys=True, indent=4)
-        try:
-            subprocess.Popen(["sync"])
-        except Exception:
-            pass
-       
+      
+    try:
+        os.rmdir("config/main")
+    except Exception:
+        pass
     
+    allConfig = copy.deepcopy(configuration)
+    main = allConfig["mainMenu"]
+    allConfig.pop('mainMenu', None)
+
+    with open('config/config.json', 'w') as fp: 
+        json.dump(allConfig, fp,sort_keys=True, indent=4)
+
+    for index, item in enumerate(main):
+        fileName = "config/main/" + str(index).zfill(3) + " " +  item["name"] + "/config.json" 
+
+        if(item["type"] == "native"):
+            data = item["data"]
+            item.pop('data', None)
+            for dataIndex, dataItem in enumerate(data):
+                dataName = "config/main/" + str(index).zfill(3) + " " +  item["name"] + "/items/" + str(dataIndex).zfill(3) + " " + dataItem["name"] + ".json"
+                storeConfigPart(dataName, dataItem)
+
+        storeConfigPart(fileName, item)
+
+       
     for l in listeners:
         l()
+       
+
+
+def storeConfigPart(fileName, item):
+    if not os.path.exists(os.path.dirname(fileName)):
+        try:
+            os.makedirs(os.path.dirname(fileName))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    with open(fileName, 'w') as fp: 
+        json.dump(item, fp,sort_keys=True, indent=4)
+
+        
 
 def getTheme():
     return theme
